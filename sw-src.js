f1e595ef6482ff62631ab0051d8674b5cfe8faf4
graff <grafservices@gmx.at>
2023-07-12 23:38:06 +0200

    updates

--- sw-src.js
+++
@@ -1,4 +0,0 @@
-import {precacheAndRoute} from 'workbox-precaching';
-
-// Hierdurch werden beim Build-Prozess generierte Assets vorgecached.
-precacheAndRoute(self.__WB_MANIFEST);
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    globDirectory: 'build/',
-    globPatterns: ['**/*.{js,css,html,png}'],
-    swSrc: 'src/sw-src.js',
-    swDest: 'build/service-worker.js',
-  };
-  
